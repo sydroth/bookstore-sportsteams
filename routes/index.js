23e1095eed6713b0ac4672d03e75320e5018698b
John Hopkins <johnthopkins@gmail.com>
2016-08-24 15:00:00 -0700

    trying to sort out index.js issue

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home-page', { title: 'Team Geek' });
+});
+
+router.get('/league/:id', function(req, res, next) {
+  res.render('nfl-detail', { title: 'NFL' });
+});
+
+router.get('/division/:id', function(req, res, next) {
+  res.render('division-detail', { title: 'Divisions' });
+});
+
+router.post('/search', function(req, res, next) {
+  res.render('search-results', { title: 'Search Results' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Log In' });
+});
+
+router.get('/logout', function(req, res, next) {
+  res.render('logout', { title: 'Log Out' });
+});
+module.exports = router;
