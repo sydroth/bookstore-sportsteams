e6d57834958b98b2eaf2e20843e93c33b3e63787
John Hopkins <johnthopkins@gmail.com>
2016-08-24 14:13:45 -0700

    Added teams routes

--- routes/index.js
+++ routes/index.js
@@ -6,36 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('home-page', { title: 'Team Geek' });
 });
 
-router.get('/nfl', function(req, res, next) {
+router.get('/league/:id', function(req, res, next) {
   res.render('nfl-detail', { title: 'NFL' });
 });
 
-router.get('/nba', function(req, res, next) {
-  res.render('nba-detail', { title: 'NBA' });
-});
-
-router.get('/mlb', function(req, res, next) {
-  res.render('mlb-detail', { title: 'MLB' });
-});
-
-router.get('/division/:division', function(req, res, next) {
+router.get('/division/:id', function(req, res, next) {
   res.render('division-detail', { title: 'Divisions' });
 });
 
-router.get('/search', function(req, res, next) {
+router.post('/search', function(req, res, next) {
   res.render('search-results', { title: 'Search Results' });
 });
 
-router.get('/team/:team', function(req, res, next) {
+router.get('/team/:id', function(req, res, next) {
   res.render('team-detail', { title: 'Team Detail' });
-});
-
-router.get('/additions/:user', function(req, res, next) {
-  res.render('my-additions', { title: 'My Additions' });
-});
-
-router.get('/add', function(req, res, next) {
-  res.render('add-team', { title: 'Add Team' });
 });
 
 router.get('/login', function(req, res, next) {
